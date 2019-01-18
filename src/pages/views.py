33aaee9b1796efab2c5dfd6912ac981b02deb1a2
ahsanhabib91 <habibibnakther@gmail.com>
2019-01-19 00:44:47 +0600

    Lec - 17

--- src/pages/views.py
+++ src/pages/views.py
@@ -1,7 +1,26 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
-
-def home_view(request):
+# Create your views here.
+def home_view(request, *args, **kwargs): # *args, **kwargs
+    print(args, kwargs)
     print(request.user)
-    return HttpResponse("<h1>asdfadfs</h1>")
+    # return HttpResponse("<h1>Hello World</h1>") # string of HTML code
+    return render(request, "home.html", {})
+
+def contact_view(request, *args, **kwargs):
+    return render(request, "contact.html", {})
+
+
+def about_view(request, *args, **kwargs):
+    my_context = {
+        "my_text": "This is about us",
+        "my_number": 123,
+        "my_list": [1313, 4231, 312, "Abc"]
+
+    }
+    return render(request, "about.html", my_context)
+
+
+def social_view(request, *args, **kwargs):
+    return HttpResponse("<h1>Socail Page</h1>")
--- src/trydjango_2_1/settings.py
+++ src/trydjango_2_1/settings.py
@@ -15,7 +15,6 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
@@ -56,7 +55,7 @@ ROOT_URLCONF = 'trydjango_2_1.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
